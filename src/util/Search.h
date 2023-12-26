a09de96d8eecad24dd003c0337b0f2a84988f80c
Gabriel Blomberg <gabriel.blomberg@appliedmining.tech>
2023-12-26 16:54:42 +1000

    Improved search architecture, fixed IDDFS. - Fixed memory leak from shared pointers in nodes pointing to eachother   and not being cleaned up on clear, by storing unique pointers to all   nodes and using raw pointers in frontier and visited. - Changed visited set to storing the search nodes, since the state   includes the search node state (such as cutoff for IDDFS). - Moved node type to global and templated search based on node type, for   reduction in complexity.

--- src/util/Search.h
+++ src/util/Search.h
@@ -5,38 +5,57 @@
 #include <memory>
 
 /**
+ * @brief A node in the search tree that contains data pertaining to the
+ * search strategy.
+ * 
+ * Examples of additional values stored in the search node include weights,
+ * state transitions and costs.
+ */
+template<typename State>
+struct SearchNode {
+
+    SearchNode() = default;
+
+    SearchNode(State state, SearchNode *parent)
+        : state(state)
+        , parent(parent)
+    {}
+
+    SearchNode(State state)
+        : state(state)
+        , parent(nullptr)
+    {}
+
+    /// The state of the node.
+    State state;
+
+    /// The parent search node visited to get to this node.
+    SearchNode *parent;
+
+    virtual ~SearchNode() = default;
+};
+
+// Hash of a search state for storing in a mapping.
+template<>
+template<typename State>
+struct std::hash<SearchNode<State>>
+{
+    std::size_t operator()(const SearchNode<State> &node) const {
+        return std::hash<State>{}(node.state);
+    }
+};
+
+/**
  * @brief A search strategy.
  * 
+ * @tparam Node the derived class of Search.
  * @tparam State The type of each
  * @tparam Compare The comparator that orders search nodes in the frontier. If
  * this is void, then a standard deque is used without insertion ordering.
  */
-template<typename State, typename Compare = void>
+template<typename Node, typename State, typename Compare = void>
 class Search {
 public:
-
-    /**
-     * @brief A node in the search tree that contains data pertaining to the
-     * search strategy.
-     * 
-     * Examples of additional values stored in the search node include weights,
-     * state transitions and costs.
-     */
-    struct Node {
-
-        Node(State state, const std::shared_ptr<Node> &predecessor)
-            : state(state)
-            , predecessor(predecessor)
-        {}
-
-        /// The state of the node.
-        State state;
-
-        /// The previous search node visited to get to this node.
-        std::shared_ptr<Node> predecessor;
-
-        virtual ~Node() = default;
-    };
 
     /**
      * @brief Function that checks if a state is the goal state.
@@ -65,12 +84,12 @@ public:
      * @return A sequence of states to the goal or std::nullopt if no solution
      * exists.
      */
-    std::optional<std::vector<std::shared_ptr<Node>>> perform();
+    std::optional<std::vector<std::unique_ptr<Node>>> perform();
 
     /**
      * @brief Get all the states visited during the search.
      */
-    inline const std::unordered_set<State> &visited() {
+    inline const std::unordered_set<Node> &visited() {
         return m_visited;
     }
 
@@ -84,36 +103,23 @@ protected:
 
     /**
      * @brief Push a search node into the frontier.
+     * 
+     * @param node The node to push onto the frontier.
+     * @returns If the node was successfully pushed onto the frontier.
      */
-    virtual void frontier_push(std::shared_ptr<Node> search_node);
+    virtual bool frontier_push(Node *node);
 
     /**
      * @brief Pop the next search node from the frontier.
      * @returns The next node to check and search.
      */
-    virtual std::shared_ptr<Node> frontier_pop();
-
-    /**
-     * @brief Make a node.
-     * 
-     * Used by derived classes to construct their own node types and cast them
-     * to a node.
-     * 
-     * @param state The state of the node.
-     * @param previous The previous node.
-     * 
-     * @return A shared pointer to a base class node.
-     */
-    virtual std::shared_ptr<Node> make_node(
-        const State &state,
-        const std::shared_ptr<Node> &previous
-    );
+    virtual Node *frontier_pop();
 
     /**
      * @brief Get all the successor search nodes from a state.
      */
-    std::vector<std::shared_ptr<Node>>
-    successors(std::shared_ptr<Node> &node);
+    std::vector<std::unique_ptr<Node>>
+    successors(Node *node);
 
     /// Function that returns the subsequent states of a state.
     Successors m_successors;
@@ -121,112 +127,123 @@ protected:
     /// Function that checks if a state is the goal state.
     Checker m_is_goal;
 
+    // All the nodes in the search tree.
+    std::vector<std::unique_ptr<Node>> m_nodes;
+
     /// The frontier of vertices to search through. A deque if not comparing
     /// search states or a priority queue if search states can be compared.
     std::conditional_t<
         std::is_void_v<Compare>,
-        std::deque<std::shared_ptr<Node>>,
-        std::priority_queue<
-            std::shared_ptr<Node>,
-            std::vector<std::shared_ptr<Node>>,
-            Compare
-        >
+        std::deque<Node*>,
+        std::priority_queue<Node*, std::vector<Node*>, Compare>
     > m_frontier;
 
+    /// Hash type to hash a node pointer using the node hash function.
+    struct Hash {
+        std::size_t operator()(const Node* const &node) const {
+            return std::hash<Node>{}(*node);
+        }
+    };
+
     // The set of visited search nodes that should not be revisited.
-    std::unordered_set<State> m_visited;
+    std::unordered_set<Node*, Hash> m_visited;
 };
 
-template<typename State, typename Compare>
-Search<State, Compare>::Search(
+template<typename Node, typename State, typename Compare>
+Search<Node, State, Compare>::Search(
         State initial,
         Successors successors,
         Checker is_goal
   ) : m_successors(successors)
     , m_is_goal(is_goal)
-    , m_frontier({make_node(initial, nullptr)})
+    , m_nodes()
+    , m_frontier()
     , m_visited()
-{}
+{
+    m_nodes.push_back(std::make_unique<Node>(initial));
+    m_frontier.push_back(m_nodes.back().get());
+}
 
-template<typename State, typename Compare>
-bool Search<State, Compare>::frontier_empty()
+template<typename Node, typename State, typename Compare>
+bool Search<Node, State, Compare>::frontier_empty()
 {
     return m_frontier.empty();
 }
 
-template<typename State, typename Compare>
-void Search<State, Compare>::frontier_push(std::shared_ptr<Node> search_node)
+template<typename Node, typename State, typename Compare>
+bool Search<Node, State, Compare>::frontier_push(Node *node)
 {
     if constexpr (std::is_void_v<Compare>) {
-        m_frontier.push_back(search_node);
+        m_frontier.push_back(node);
     }
     else {
-        m_frontier.push(search_node);
+        m_frontier.push(node);
     }
+
+    return true;
 };
 
-template<typename State, typename Compare>
-std::shared_ptr<typename Search<State, Compare>::Node>
-Search<State, Compare>::frontier_pop()
+template<typename Node, typename State, typename Compare>
+Node *Search<Node, State, Compare>::frontier_pop()
 {
-    auto node = std::move(m_frontier.front());
+    Node *node = m_frontier.front();
     m_frontier.pop_front();
-    return std::move(node);
+    return node;
 }
 
-template<typename State, typename Compare>
-std::shared_ptr<typename Search<State, Compare>::Node>
-Search<State, Compare>::make_node(
-    const State &state,
-    const std::shared_ptr<Node> &previous
-) {
-    return std::make_shared<Node>(state, previous);
-}
-
-template<typename State, typename Compare>
-std::vector<std::shared_ptr<typename Search<State, Compare>::Node>>
-Search<State, Compare>::successors(std::shared_ptr<Node> &node)
+template<typename Node, typename State, typename Compare>
+std::vector<std::unique_ptr<Node>>
+Search<Node, State, Compare>::successors(Node *node)
 {
     // The states that are immediately reachable from state.
     std::vector<State> next_states = m_successors(node->state);
 
     // Construct the search nodes of each new state.
-    std::vector<std::shared_ptr<Node>> nodes {};
+    std::vector<std::unique_ptr<Node>> nodes {};
     nodes.reserve(next_states.size());
 
-    for (const State &next : next_states) {
-        nodes.push_back(make_node(next, node));
-    }
+    for (const State &next : next_states)
+        nodes.push_back(std::make_unique<Node>(next, node));
 
     return std::move(nodes);
 }
 
-template<typename State, typename Compare>
-std::optional<std::vector<std::shared_ptr<typename Search<State, Compare>::Node>>>
-Search<State, Compare>::perform()
+template<typename Node, typename State, typename Compare>
+std::optional<std::vector<std::unique_ptr<Node>>>
+Search<Node, State, Compare>::perform()
 {
-    while (!m_frontier.empty()) {
-        std::shared_ptr<typename Search<State, void>::Node> node = frontier_pop();
+    while (!frontier_empty()) {
+        Node *node = frontier_pop();
 
         // If its the not the goal then mark this node visited and add its
         // subsequent search nodes to the frontier.
         if (!m_is_goal(node->state)) {
-            m_visited.emplace(node->state);
+            m_visited.emplace(node);
 
-            for (auto &next : successors(node))
-                if (m_visited.find(next->state) == m_visited.end())
-                    frontier_push(next);
+            for (auto &next : successors(node)) {
+                auto pointer = next.get();
+
+                // Only push search states that have not been visited.
+                if (m_visited.find(pointer) == m_visited.end()) {
+
+                    // If the node was pushed onto the frontier then maintain
+                    // the unique pointer to it.
+                    if (frontier_push(pointer))
+                        m_nodes.push_back(std::move(next));
+                }
+            }
 
             continue;
         }
 
         // Found the goal, traverse backwards to generate the sequence of
         // search nodes visited to get to the goal
-        std::vector<std::shared_ptr<Node>> trace {};
-        for (; node; node = node->predecessor)
-            trace.push_back(node);
+        std::vector<std::unique_ptr<Node>> trace {};
+        for (; node; node = node->parent)
+            trace.push_back(std::make_unique<Node>(*node));
 
         // For memory saving.
+        m_nodes.clear();
         m_frontier.clear();
         m_visited.clear();
 
@@ -242,18 +259,19 @@ Search<State, Compare>::perform()
  * @brief Depth first search. Search tree traversed breadth first.
  */
 template<typename State>
-using BFS = Search<State, void>;
+class BFS : public Search<SearchNode<State>, State, void>
+{};
 
 /**
  * @brief Depth first search. Search tree traversed depth first.
  */
 template<typename State>
-class DFS : public Search<State, void>
+class DFS : public Search<SearchNode<State>, State, void>
 {
 public:
 
-    using Search<State, void>::Node;
-    using Search<State, void>::Search;
+    using Node = SearchNode<State>;
+    using Search<Node, State, void>::Search;
 
 private:
 
@@ -261,77 +279,94 @@ private:
      * @brief Pop the newest search node added to the frontier.
      * @returns The newest node on the frontier.
      */
-    std::shared_ptr<typename Search<State, void>::Node> frontier_pop() override;
+    Node *frontier_pop() override;
 
-    using Search<State, void>::m_frontier;
+    using Search<Node, State, void>::m_frontier;
 };
 
 template<typename State>
-std::shared_ptr<typename Search<State, void>::Node>
-DFS<State>::frontier_pop()
+typename DFS<State>::Node* DFS<State>::frontier_pop()
 {
-    auto node = std::move(m_frontier.back());
+    auto node = m_frontier.back();
     m_frontier.pop_back();
-    return std::move(node);
+    return node;
 }
+
+/**
+ * @brief An IDDFS node keeps track of its height in the search tree.
+ */
+template<typename State>
+struct IDDFSNode {
+
+    IDDFSNode() = default;
+
+    IDDFSNode(
+            State state,
+            IDDFSNode<State> *parent
+      ) : state(state)
+        , parent(parent)
+        , k(parent->k + 1)
+    {}
+
+    IDDFSNode(State state)
+        : state(state)
+        , parent(nullptr)
+        , k(0)
+    {}
+
+    State state;
+
+    IDDFSNode<State> *parent;
+
+    /// The height of the search node in the search tree.
+    int k;
+};
+
+// Hash of a search state for storing in a mapping.
+template<>
+template<typename State>
+struct std::hash<IDDFSNode<State>>
+{
+    std::size_t operator()(const IDDFSNode<State> &node) const {
+        return std::hash<State>{}(node.state) ^ (std::hash<int>{}(node.k) << 1);
+    }
+};
 
 /**
  * @brief Iteratively deepening depth first search. 
  */
 template<typename State>
-class IDDFS : public Search<State, void>
+class IDDFS : public Search<IDDFSNode<State>, State, void>
 {
 public:
 
-    /**
-     * @brief An IDDFS node keeps track of its height in the search tree.
-     */
-    struct Node : public Search<State, void>::Node {
-
-        Node(
-            State state,
-            std::shared_ptr<typename Search<State, void>::Node> previous,
-            int k
-          ) : Search<State, void>::Node(state, previous)
-            , k(k)
-        {}
-
-        /// The height of the search node in the search tree.
-        int k;
-    };
+    using Node = IDDFSNode<State>;
 
     IDDFS(
             State initial,
-            Search<State, void>::Successors successor,
-            Search<State, void>::Checker is_goal
-      ) : Search<State, void>(initial, successor, is_goal)
+            Search<Node, State, void>::Successors successor,
+            Search<Node, State, void>::Checker is_goal
+      ) : Search<Node, State, void>(initial, successor, is_goal)
         , m_initial_state(initial)
         , m_cutoff(3)
         , m_deepen(false)
-        , m_depths()
     {}
 
 private:
 
-    std::shared_ptr<typename Search<State, void>::Node> make_node(
-        const State &state,
-        const std::shared_ptr<typename Search<State, void>::Node> &previous
-    ) override;
-
     bool frontier_empty() override;
 
-    void frontier_push(
-        std::shared_ptr<typename Search<State, void>::Node> search_node
-    ) override;
+    bool frontier_push(Node *node) override;
 
     /**
      * @brief Pop the newest search node added to the frontier.
      * @returns The newest node on the frontier.
      */
-    std::shared_ptr<typename Search<State, void>::Node> frontier_pop() override;
+    Node *frontier_pop() override;
 
-    using Search<State, void>::m_frontier;
-    using Search<State, void>::m_visited;
+    using Search<Node, State, void>::m_nodes;
+    using Search<Node, State, void>::m_frontier;
+    using Search<Node, State, void>::m_visited;
 
     /// Cache of the initial state for restarts.
     State m_initial_state;
@@ -341,38 +376,29 @@ private:
 
     /// Whether to restart the search.
     bool m_deepen;
-
-    /// Record of the visited state depths.
-    std::unordered_map<State, int> m_depths;
-};
-
-template<typename State>
-std::shared_ptr<typename Search<State, void>::Node>
-IDDFS<State>::make_node(
-    const State &state,
-    const std::shared_ptr<typename Search<State, void>::Node> &previous
-) {
-    const Node *node = (Node*)previous.get();
-    return std::make_shared<Node>(
-        state,
-        previous,
-        previous ? node->k + 1 : 0
-    );
 };
 
 template<typename State>
 bool IDDFS<State>::frontier_empty()
 {
-    if (m_frontier.empty())
+    if (!m_frontier.empty())
         return false;
 
+    // If the frontier is empty and the depth limit was reached, restart with
+    // a deeper index.
     if (m_deepen) {
         m_deepen = false;
+
+        // Clear old search tree.
+        m_nodes.clear();
         m_frontier.clear();
         m_visited.clear();
-        m_depths.clear();
-        m_frontier.push_back(make_node(m_initial_state, nullptr));
+
+        // Instantiate the new search tree.
+        m_nodes.push_back(std::move(std::make_unique<Node>(m_initial_state)));
+        m_frontier.push_back(m_nodes.back().get());
         m_cutoff += 1;
+
         return false;
     }
 
@@ -380,33 +406,22 @@ bool IDDFS<State>::frontier_empty()
 }
 
 template<typename State>
-void IDDFS<State>::frontier_push(
-    std::shared_ptr<typename Search<State, void>::Node> search_node
-) {
-    const Node *node = dynamic_cast<Node*>(search_node.get());
-
-    // If the search depth has been reached then deepen.
+bool IDDFS<State>::frontier_push(Node *node)
+{
+    // If the search depth has been reached then increase the maximum tree depth
+    // once the frontier becomes empty.
     if (node->k > m_cutoff) {
         m_deepen = true;
-        return;
+        return false;
     }
 
-    // We should push the node to the frontier if this search node has a
-    // shallower depth than the last search node for this state.
-    auto previous_depth = m_depths.find(node->state);
-    if (previous_depth != m_depths.end() && node->k >= previous_depth->second)
-        return;
-
-    m_depths[node->state] = node->k;
-
-    return Search<State, void>::frontier_push(search_node);
+    return Search<Node, State, void>::frontier_push(node);
 }
 
 template<typename State>
-std::shared_ptr<typename Search<State, void>::Node>
-IDDFS<State>::frontier_pop()
+typename IDDFS<State>::Node *IDDFS<State>::frontier_pop()
 {
-    auto node = std::move(m_frontier.back());
+    auto node = m_frontier.back();
     m_frontier.pop_back();
-    return std::move(node);
+    return node;
 }
